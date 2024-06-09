36c21d9caa792b04703509d6af59e30384436047
Jhonatan Rodriguez <jhonathanrodriguez157@gmail.com>
2024-06-09 00:31:12 -0500

    style: adjust padding in main activity and update readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # timetonic-android-book-app
-A simple Android app for fetching and displaying books using Timetonic's public API. Includes a login page and landing page.
+
+A simple Android app for fetching and displaying books using Timetonic's public API. Includes a
+login page and landing page.
+
+## Video Demo
+
+<p align="center">
+  <a href="https://streamable.com/l2xlau">
+    <img src="https://img.youtube.com/vi/V-Z-w-b-Q9s/maxresdefault.jpg" alt="Demo" style="display:block; margin:auto; width:50%;" />
+  </a>
+</p>
+
+## Dependencies
+
+- Jetpack Compose: Modern toolkit for building native Android UI.
+- Retrofit: HTTP client for making network requests.
+- Coil: Image loading library for Android.
+- Kotlin Serialization: Library for serializing and deserializing Kotlin objects.
+- AndroidX Libraries: Core, Lifecycle, Activity, Navigation, Material3, etc.
+
+## Features
+
+- Login page with email and password fields.
+- Landing page with a list of books.
--- app/src/main/java/com/timetonic/booklistapp/MainActivity.kt
+++ app/src/main/java/com/timetonic/booklistapp/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -20,7 +25,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BookListAppTheme {
-                MainAppNavHost()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(BookListAppTheme.colorScheme.background)
+                        .statusBarsPadding()
+                ) {
+                    MainAppNavHost()
+                }
             }
         }
     }
